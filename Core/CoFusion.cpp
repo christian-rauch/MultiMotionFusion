f9e0800f8fa38f8393c6ccf65e8d62036c2a97a4
Christian Rauch <Christian.Rauch@ed.ac.uk>
2020-07-13 17:00:40 +0100

    show all models

--- Core/CoFusion.cpp
+++ Core/CoFusion.cpp
@@ -190,13 +190,36 @@ bool CoFusion::processFrame(const FrameData& frame, const Eigen::Matrix4f* inPos
   Eigen::MatrixX2d coordinates;
   Eigen::MatrixXd descriptors;
   std::tie(std::ignore, coordinates, descriptors) = sp.getFeatures(frame.rgb);
-//  cv::Mat img;
-//  cv::cvtColor(frame.rgb, img, cv::COLOR_RGB2GRAY);
-//  for(int i=0; i<coordinates.rows(); i++) {
-//      cv::circle(img, cv::Point(coordinates(i,0)*img.cols, coordinates(i,1)*img.rows), 5, cv::Scalar(255));
-//  }
-//  cv::imshow("img", img);
+  cv::Mat img;
+  cv::cvtColor(frame.rgb, img, cv::COLOR_RGB2GRAY);
+  for(int i=0; i<coordinates.rows(); i++) {
+      cv::circle(img, cv::Point(coordinates(i,0)*img.cols, coordinates(i,1)*img.rows), 5, cv::Scalar(255));
+  }
+  cv::imshow("current observation", img);
 //  cv::waitKey(1);
+
+  // rendered state at t-1
+  int i=0;
+  for(const auto &model : models) {
+      const cv::Mat proj = model->getRGBProjection()->downloadTexture();
+
+      Eigen::MatrixX2d coordinates;
+      Eigen::MatrixXd descriptors;
+      std::tie(std::ignore, coordinates, descriptors) = sp.getFeatures(proj);
+
+      cv::Mat img;
+      cv::cvtColor(proj, img, cv::COLOR_RGB2GRAY);
+      for(int i=0; i<coordinates.rows(); i++) {
+          cv::circle(img, cv::Point(coordinates(i,0)*img.cols, coordinates(i,1)*img.rows), 5, cv::Scalar(255));
+      }
+      const std::string win_name = "estimated model "+std::to_string(i);
+      cv::namedWindow(win_name, cv::WINDOW_NORMAL);
+      cv::imshow(win_name, img);
+
+      i++;
+  }
+  cv::waitKey(1);
+
   TOCK("Keypoints");
 
   TICK("Preprocess");
