b31f05eabef9de2d021c6a2b34173db559e86b7e
Christian Rauch <Christian.Rauch@ed.ac.uk>
2021-05-20 12:22:03 +0100

    inhibit service

--- GUI/Tools/RosInterface.cpp
+++ GUI/Tools/RosInterface.cpp
@@ -8,6 +8,8 @@ RosInterface::RosInterface(GUI **gui)
   n = std::make_unique<ros::NodeHandle>("~");
 
   srv_reset = n->advertiseService("reset", &RosInterface::on_reset, this);
+
+  srv_inhibit = n->advertiseService("inhibit", &RosInterface::on_inhibit, this);
 }
 
 bool RosInterface::on_reset(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
@@ -15,6 +17,30 @@ bool RosInterface::on_reset(std_srvs::Trigger::Request &req, std_srvs::Trigger::
   (*gui)->reset->Ref()->Set(true);
   res.success = true;
   res.message = "reset map and models";
+  return true;
+}
+
+bool RosInterface::on_inhibit(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res)
+{
+  const std::string action = req.data ? "inhibited" : "allowed";
+
+  const bool apply_change = (*gui)->inhibitModels->Get() != req.data;
+
+  if (!apply_change) {
+    res.success = false;
+    res.message = "inhibit settings not applied: spawning new models is already " + action;
+  }
+  else {
+    (*gui)->inhibitModels->Ref()->Set(req.data);
+    res.success = (*gui)->inhibitModels->Get() == req.data;
+    if (res.success) {
+      res.message = "spawning new models will be " + action;
+    }
+    else {
+      res.message = "could not apply inhibit setting";
+    }
+  }
+
   return true;
 }
 
--- GUI/Tools/RosInterface.hpp
+++ GUI/Tools/RosInterface.hpp
@@ -2,6 +2,7 @@
 
 #include <ros/ros.h>
 #include <std_srvs/Trigger.h>
+#include <std_srvs/SetBool.h>
 
 // manually include some headers here, since they are missing from 'GUI.h'
 #include <list>
@@ -15,6 +16,8 @@ public:
 
     bool on_reset(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
 
+    bool on_inhibit(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res);
+
 private:
     // pointer to a pointer to the GUI, since the GUI will not have been
     // allocated yet when this class is constructed
@@ -22,4 +25,5 @@ private:
 
     std::unique_ptr<ros::NodeHandle> n;
     ros::ServiceServer srv_reset;
+    ros::ServiceServer srv_inhibit;
 };
