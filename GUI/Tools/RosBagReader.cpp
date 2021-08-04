569d7cade7f32e3dbddc367e50d484220f23f392
Christian Rauch <Christian.Rauch@ed.ac.uk>
2021-08-04 15:16:39 +0100

    provide ground truth poses via rosbag tf

--- GUI/Tools/RosBagReader.cpp
+++ GUI/Tools/RosBagReader.cpp
@@ -4,16 +4,21 @@
 #include "ros_common.hpp"
 #include <sensor_msgs/CameraInfo.h>
 #include <cv_bridge/cv_bridge.h>
-#include <Eigen/Core>
+#include <tf2_msgs/TFMessage.h>
+#include <tf2/exceptions.h>
+#include <tf2_eigen/tf2_eigen.h>
 
 RosBagReader::RosBagReader(const std::string bagfile_path,
                            const std::string topic_colour,
                            const std::string topic_depth,
                            const std::string topic_camera_info,
                            const bool flipColors,
-                           const cv::Size target_dimensions) :
+                           const cv::Size target_dimensions,
+                           const std::string frame_gt_camera) :
   LogReader(bagfile_path, flipColors), target_dimensions(target_dimensions),
-  topic_colour(topic_colour), topic_depth(topic_depth), topic_camera_info(topic_camera_info)
+  topic_colour(topic_colour), topic_depth(topic_depth), topic_camera_info(topic_camera_info),
+  frame_gt_camera(frame_gt_camera),
+  tf_buffer(ros::Duration(std::numeric_limits<int>::max()))
 {
   bag.open(bagfile_path, rosbag::bagmode::Read);
 
@@ -45,6 +50,14 @@ RosBagReader::RosBagReader(const std::string bagfile_path,
 
   calibrationFile = {};
 
+  // cache all transformations
+  const bool has_tf_dyn = add_all_tf_msgs("/tf", false);
+  const bool has_tf_sta = add_all_tf_msgs("/tf_static", true);
+  has_tf = has_tf_dyn || has_tf_sta;
+
+  if (!frame_gt_camera.empty() && !tf_buffer._frameExists(frame_gt_camera))
+    throw std::runtime_error("provided ground truth camera frame '" + frame_gt_camera + "' does not exist");
+
   topic_view.addQuery(bag, rosbag::TopicQuery({topic_colour, topic_depth}));
 
   if (topic_view.size() == 0)
@@ -61,29 +74,59 @@ void RosBagReader::getNext() {
   // reset data
   data = {};
 
+  std_msgs::Header hdr_colour;
+  std_msgs::Header hdr_depth;
+
   for(bool has_colour = false, has_depth = false;
       !(has_colour & has_depth) & hasMore();
       iter_msg++)
   {
     if(iter_msg->getTopic()==topic_colour) {
       if (const auto m = iter_msg->instantiate<sensor_msgs::CompressedImage>()) {
-        data.timestamp = int64_t(m->header.stamp.toNSec());
+        hdr_colour = m->header;
         data.rgb = cv_bridge::toCvCopy(m, "rgb8")->image;
       }
       else if (const auto m = iter_msg->instantiate<sensor_msgs::Image>()) {
-        data.timestamp = int64_t(m->header.stamp.toNSec());
+        hdr_colour = m->header;
         data.rgb = cv_bridge::toCvCopy(m, "rgb8")->image;
       }
       else {
         throw std::runtime_error("colour topic '" + topic_colour + "' must only contain messages of type 'sensor_msgs/Image' or 'sensor_msgs/CompressedImage'");
       }
-      has_colour = true;
+
+      data.timestamp = int64_t(hdr_colour.stamp.toNSec());
+
+      if (has_tf) {
+        // use provided ground truth camera frame or colour optical frame from images
+        if (frame_gt_camera.empty())
+          frame_gt_camera = hdr_colour.frame_id;
+
+        // find root frame
+        if (frame_gt_root.empty()) {
+          std::string parent = frame_gt_camera;
+          while (tf_buffer._getParent(parent, {}, parent));
+          frame_gt_root = parent;
+        }
+
+        try {
+          poses[data.timestamp] = tf2::transformToEigen(tf_buffer.lookupTransform(frame_gt_root, frame_gt_camera, hdr_colour.stamp));
+          has_colour = true;
+        } catch (tf2::ExtrapolationException &) {
+          // we have to wait for a colour frame with a matching ground truth transformation
+          has_colour = false;
+        }
+      }
+      else {
+        has_colour = true;
+      }
     }
     else if(iter_msg->getTopic()==topic_depth) {
       if (const auto m = iter_msg->instantiate<sensor_msgs::CompressedImage>()) {
+        hdr_depth = m->header;
         data.depth = cv_bridge::toCvCopy(m)->image;
       }
       else if (const auto m = iter_msg->instantiate<sensor_msgs::Image>()) {
+        hdr_depth = m->header;
         data.depth = cv_bridge::toCvCopy(m)->image;
       }
       else {
@@ -102,6 +145,9 @@ void RosBagReader::getNext() {
 
   if (hasMore() && data.depth.empty())
     throw std::runtime_error("no images on depth topic '" + topic_depth + "'");
+
+  if (hasMore() && (hdr_colour.frame_id != hdr_depth.frame_id))
+    throw std::runtime_error("colour and depth images are not registered into the same frame");
 
   // scale and crop images in place
   if (scale_colour && !data.rgb.empty()) {
@@ -143,5 +189,33 @@ void RosBagReader::setAuto(bool /*value*/) {
 FrameData RosBagReader::getFrameData() {
   return data;
 };
+
+Eigen::Matrix4f RosBagReader::getIncrementalTransformation(uint64_t timestamp) {
+  if (!has_tf)
+    throw std::runtime_error("rosbag has no ground truth camera poses");
+
+  if (!timestamp)
+    return {};
+
+  // provide camera poses with respect to the reference pose (first pose in the trajectory)
+  if (ref_time==0) {
+    ref_time = timestamp;
+  }
+  return (poses.at(ref_time).inverse() * poses.at(timestamp)).matrix().cast<float>();
+};
+
+bool RosBagReader::add_all_tf_msgs(const std::string &topic, const bool tf_static) {
+  size_t nvalid = 0;
+  for (const rosbag::MessageInstance &m : rosbag::View(bag, rosbag::TopicQuery(topic))) {
+    if (const tf2_msgs::TFMessage::ConstPtr &mi = m.instantiate<tf2_msgs::TFMessage>()) {
+      for (const geometry_msgs::TransformStamped &tf : mi->transforms) {
+        if (tf_buffer.setTransform(tf, {}, tf_static)) {
+          nvalid++;
+        }
+      }
+    }
+  }
+  return nvalid>0;
+}
 
 #endif
--- GUI/Tools/RosBagReader.hpp
+++ GUI/Tools/RosBagReader.hpp
@@ -5,15 +5,19 @@
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
 #include <sensor_msgs/CompressedImage.h>
+#include <tf2/buffer_core.h>
+#include <Utils/GroundTruthOdometryInterface.hpp>
+#include <Eigen/Geometry>
 
-class RosBagReader : public LogReader {
+class RosBagReader : public LogReader, public GroundTruthOdometryInterface {
 public:
   RosBagReader(const std::string bagfile_path,
                const std::string topic_colour,
                const std::string topic_depth,
                const std::string topic_camera_info,
                const bool flipColors = false,
-               const cv::Size target_dimensions = {});
+               const cv::Size target_dimensions = {},
+               const std::string frame_gt_camera = {});
 
   ~RosBagReader();
 
@@ -35,6 +39,8 @@ public:
 
   FrameData getFrameData() override;
 
+  Eigen::Matrix4f getIncrementalTransformation(uint64_t timestamp) override;
+
 private:
   rosbag::Bag bag;
   rosbag::View topic_view;
@@ -50,7 +56,17 @@ private:
   const std::string topic_depth;
   const std::string topic_camera_info;
 
+  // ground truth camera poses in root frame
+  std::string frame_gt_root;
+  std::string frame_gt_camera;
+  tf2::BufferCore tf_buffer;
+  bool has_tf;
+  std::map<uint64_t, Eigen::Isometry3d> poses;
+  uint64_t ref_time = 0;
+
   FrameData data;
+
+  bool add_all_tf_msgs(const std::string &topic, const bool tf_static);
 };
 
 #endif
