7e262db676101e740f9ffd4e53dc12a0c97031d1
Christian Rauch <Rauch.Christian@gmx.de>
2023-12-28 12:48:23 +0100

    install dependencies that may be missing on barebone systems

--- doc/setup.sh
+++ doc/setup.sh
@@ -10,13 +10,14 @@ sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/
 sudo apt install --no-install-recommends -y cuda-libraries-dev-11-3 cuda-compiler-11-3 cuda-nvtx-11-3 libcudnn8-dev
 
 echo "install ROS"
+sudo apt install -y software-properties-common curl
+sudo add-apt-repository -y universe
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt install --no-install-recommends -y ros-noetic-ros-base
+sudo -E apt install --no-install-recommends -y ros-noetic-ros-base ros-dev-tools
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 
-echo "install vcstool, rosdep, colcon"
-sudo pip3 install -U vcstool rosdep colcon-common-extensions
+echo "initialise rosdep"
 sudo rosdep init
 rosdep update
