67f313647c433a3793bf22f3a832f4ed45414e73
Christian Rauch <Christian.Rauch@unileoben.ac.at>
2025-05-13 14:22:31 +0200

    port 'RosStatePublisher' to ROS 2

--- GUI/Tools/RosStatePublisher.hpp
+++ GUI/Tools/RosStatePublisher.hpp
@@ -1,8 +1,19 @@
 #pragma once
 
 #include <memory>
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
+#if defined(ROS1)
+    #include <ros/ros.h>
+    #include <image_transport/image_transport.h>
+#elif defined(ROS2)
+    #include <rclcpp/rclcpp.hpp>
+    #include <image_transport/image_transport.hpp>
+    #include <sensor_msgs/msg/image.hpp>
+    #include <sensor_msgs/msg/compressed_image.hpp>
+    #include <sensor_msgs/msg/point_cloud2.hpp>
+    #include <std_msgs/msg/string.hpp>
+    using namespace sensor_msgs::msg;
+    using namespace std_msgs::msg;
+#endif
 #include <tf2_ros/transform_broadcaster.h>
 #include <opencv2/core.hpp>
 #include <Model/Model.h>
@@ -21,17 +32,31 @@ public:
     void send_status_message(const std::string &message);
 
 private:
+#if defined(ROS1)
     std::unique_ptr<ros::NodeHandle> n;
-    std::unique_ptr<image_transport::ImageTransport> it;
     tf2_ros::TransformBroadcaster broadcaster;
+#elif defined(ROS2)
+    rclcpp::Node::SharedPtr n;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> broadcaster;
+#endif
+    std::unique_ptr<image_transport::ImageTransport> it;
 
     image_transport::Publisher pub_segm;
+#if defined(ROS1)
     ros::Publisher pub_camera_info;
     std::unordered_map<uint8_t, ros::Publisher> pub_camera_info_depth;
     std::unordered_map<uint8_t, ros::Publisher> pub_model_pc;
     std::unordered_map<uint8_t, ros::Publisher> pub_model_proj_colour;
     std::unordered_map<uint8_t, ros::Publisher> pub_model_proj_depth;
     ros::Publisher pub_status_message;
+#elif defined(ROS2)
+    rclcpp::Publisher<CameraInfo>::SharedPtr pub_camera_info;
+    std::unordered_map<uint8_t, rclcpp::Publisher<CameraInfo>::SharedPtr> pub_camera_info_depth;
+    std::unordered_map<uint8_t, rclcpp::Publisher<PointCloud2>::SharedPtr> pub_model_pc;
+    std::unordered_map<uint8_t, rclcpp::Publisher<CompressedImage>::SharedPtr> pub_model_proj_colour;
+    std::unordered_map<uint8_t, rclcpp::Publisher<CompressedImage>::SharedPtr> pub_model_proj_depth;
+    rclcpp::Publisher<String>::SharedPtr pub_status_message;
+#endif
 
     const std::string camera_frame;
 };
